d0b9645c5af124fd33108d1876e4c62dc7c1c9e6
Punit Mudgal <ssmudgal01@gmail.com>
2023-02-26 17:25:37 +0530

    header part

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='header section_padding' id='home'>
+<div className='header-content'>
+  <h1 className='gradient_text'>Let's Build Something amazing with GPT-3 OpenAI</h1>
+  <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+<div className='header-content-input'>
+  <input type="email" placeholder='Your Email Address'/>
+  <button type='button'>Get Started</button>
+</div>
+<div className='header-content-people'>
+    <img src={people} alt="not found!" />
+    <p>1,600 people requested access a visit in last 24 hours</p>
+</div>
+</div>
+<div className='header-content-ai'>
+  <img src={ai} alt="not found" />
+</div>
+    </div>
   )
 }
